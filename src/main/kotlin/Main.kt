d051afe89f98e29ef38ad02de122a91f0102edd5
Mike Neilens <mike.neilens@gmail.com>
2023-03-27 14:50:07 +0100

    initial

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,38 +6,37 @@ interface TrafficLightState {
 fun changeColors(trafficLightState: TrafficLightState, numberOfChanges:Int):TrafficLightState =
      (1..numberOfChanges).fold(trafficLightState){tls, _ -> tls.changeColour() }
 
-object Red:TrafficLightState {
-    override fun changeColour():RedAmber = RedAmber
+class Red:TrafficLightState {
+    override fun changeColour():RedAmber = RedAmber()
     override fun toString() = "I am Red"
 }
 
-object RedAmber:TrafficLightState {
-    override fun changeColour():Green = Green
+class RedAmber:TrafficLightState {
+    override fun changeColour():Green = Green()
     override fun toString() = "I am Red and Amber"
 }
 
-object Green:TrafficLightState {
-    override fun changeColour():Amber = Amber
+class Green:TrafficLightState {
+    override fun changeColour():Amber = Amber()
     override fun toString() = "I am Green"
 }
 
-object Amber:TrafficLightState {
-    override fun changeColour():Red = Red
+class Amber:TrafficLightState {
+    override fun changeColour():Red = Red()
     override fun toString() = "I am Amber"
 }
 
 
 //If Red had more than one possible output then we could use something like this
-//n.b. Once Red2 has been changed several times it turns back into a standard red traffic light!
-class Red2(val shouldSkipAmber:Boolean):TrafficLightState {
-    override fun changeColour():RedOutput = if (shouldSkipAmber) RedOutput.GreenOption else RedOutput.RedAmberOption
+//n.b. Once RedVersion2 has been changed several times it turns back into a standard red traffic light!
+class RedVersion2(val shouldSkipAmber:Boolean):TrafficLightState {
+    override fun changeColour():RedOutput = if (shouldSkipAmber) RedOutput.GreenOption() else RedOutput.RedAmberOption()
     override fun toString() = "I am Red but can turn to red/amber or go straight to green"
 }
 
 sealed class RedOutput(val trafficLightState: TrafficLightState):TrafficLightState {
-    object RedAmberOption:RedOutput(RedAmber)
-    object GreenOption:RedOutput(Green)
+    class RedAmberOption:RedOutput(RedAmber())
+    class GreenOption:RedOutput(Green())
     override fun changeColour(): TrafficLightState = this.trafficLightState.changeColour()
 }
-
 
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,10 +1,9 @@
 import io.kotest.core.spec.style.WordSpec
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
 
 class MainTest:WordSpec ({
     "a red traffic light" should  ({
-        val redTrafficLight = Red
+        val redTrafficLight = Red()
         "when changed once should turn red/amber" {
             changeColors(redTrafficLight,1).shouldBeTypeOf<RedAmber>()
         }
@@ -20,19 +19,19 @@ class MainTest:WordSpec ({
     })
     "a red traffic light that can sometimes go straight to green" should ({
         "when changed once should not skip amber if flag not set" {
-            val redTrafficLight = Red2(shouldSkipAmber = false)
+            val redTrafficLight = RedVersion2(shouldSkipAmber = false)
             changeColors(redTrafficLight,1).shouldBeTypeOf <RedOutput.RedAmberOption>()
         }
         "when changed once should skip amber if flag is set" {
-            val redTrafficLight = Red2(shouldSkipAmber = true)
+            val redTrafficLight = RedVersion2(shouldSkipAmber = true)
             changeColors(redTrafficLight,1).shouldBeTypeOf <RedOutput.GreenOption>()
         }
         "when changed two times turns green if skip amber flag not set" {
-            val redTrafficLight = Red2(shouldSkipAmber = false)
+            val redTrafficLight = RedVersion2(shouldSkipAmber = false)
             changeColors(redTrafficLight,2).shouldBeTypeOf<Green>()
         }
         "when changed two times turns amber if skip amber flag set" {
-            val redTrafficLight = Red2(shouldSkipAmber = true)
+            val redTrafficLight = RedVersion2(shouldSkipAmber = true)
             changeColors(redTrafficLight,2).shouldBeTypeOf<Amber>()
         }
 
