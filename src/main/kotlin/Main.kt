697fbefc3326960abad7739e1bd6aac7757a99c0
Mike Neilens <mike.neilens@gmail.com>
2023-03-27 16:59:37 +0100

    initial

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,37 +6,46 @@ interface TrafficLightState {
 fun changeColors(trafficLightState: TrafficLightState, numberOfChanges:Int):TrafficLightState =
      (1..numberOfChanges).fold(trafficLightState){tls, _ -> tls.changeColour() }
 
-class Red:TrafficLightState {
-    override fun changeColour():RedAmber = RedAmber()
+class Red(val fixture:Int):TrafficLightState {
+    override fun changeColour():RedAmber = RedAmber(fixture)
     override fun toString() = "I am Red"
 }
 
-class RedAmber:TrafficLightState {
-    override fun changeColour():Green = Green()
+class RedAmber(val fixture:Int):TrafficLightState {
+    override fun changeColour():Green = Green(fixture)
     override fun toString() = "I am Red and Amber"
 }
 
-class Green:TrafficLightState {
-    override fun changeColour():Amber = Amber()
+class Green(val fixture:Int):TrafficLightState {
+    override fun changeColour():Amber = Amber(fixture)
     override fun toString() = "I am Green"
 }
 
-class Amber:TrafficLightState {
-    override fun changeColour():Red = Red()
+class Amber(val fixture:Int):TrafficLightState {
+    override fun changeColour():Red = Red(fixture)
     override fun toString() = "I am Amber"
 }
 
 
 //If Red had more than one possible output then we could use something like this
 //n.b. Once RedVersion2 has been changed several times it turns back into a standard red traffic light!
-class RedVersion2(val shouldSkipAmber:Boolean):TrafficLightState {
-    override fun changeColour():RedOutput = if (shouldSkipAmber) RedOutput.GreenOption() else RedOutput.RedAmberOption()
+class RedVersion2(val fixture:Int, val shouldSkipAmber:Boolean):TrafficLightState {
+    override fun changeColour():RedOutput =
+        if (fixture == 0) RedOutput.BrokenOption(fixture, "Fixture 0 doesn't work")
+        else if (shouldSkipAmber) RedOutput.GreenOption(fixture)
+        else RedOutput.RedAmberOption(fixture)
     override fun toString() = "I am Red but can turn to red/amber or go straight to green"
 }
 
 sealed class RedOutput(val trafficLightState: TrafficLightState):TrafficLightState {
-    class RedAmberOption:RedOutput(RedAmber())
-    class GreenOption:RedOutput(Green())
+    class RedAmberOption(val fixture:Int):RedOutput(RedAmber(fixture))
+    class GreenOption(val fixture:Int):RedOutput(Green(fixture))
+    class BrokenOption(val fixture:Int, val reason:String):RedOutput(Broken(fixture, reason))
     override fun changeColour(): TrafficLightState = this.trafficLightState.changeColour()
+}
+
+class Broken(val fixture:Int, val reason:String):TrafficLightState {
+    override fun changeColour():Broken = Broken(fixture, "Still broken")
+    override fun toString() = "I am Broken"
 }
 
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,38 +1,54 @@
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
 
 class MainTest:WordSpec ({
     "a red traffic light" should  ({
-        val redTrafficLight = Red()
+        val redTrafficLight = Red(fixture = 1234)
         "when changed once should turn red/amber" {
-            changeColors(redTrafficLight,1).shouldBeTypeOf<RedAmber>()
+            val newTrafficLight = changeColors(redTrafficLight,1)
+            newTrafficLight.shouldBeTypeOf<RedAmber>()
+            newTrafficLight.fixture shouldBe 1234
         }
         "when changed two times turns green" {
-            changeColors(redTrafficLight,2).shouldBeTypeOf<Green>()
+            val newTrafficLight = changeColors(redTrafficLight,2)
+            newTrafficLight.shouldBeTypeOf<Green>()
+            newTrafficLight.fixture shouldBe 1234
         }
         "when changed three times turns amber" {
-            changeColors(redTrafficLight,3).shouldBeTypeOf<Amber>()
+            val newTrafficLight = changeColors(redTrafficLight,3)
+            newTrafficLight.shouldBeTypeOf<Amber>()
+            newTrafficLight.fixture shouldBe 1234
         }
         "when changed four times turns red" {
-            changeColors(redTrafficLight,4).shouldBeTypeOf<Red>()
+            val newTrafficLight = changeColors(redTrafficLight,4)
+            newTrafficLight.shouldBeTypeOf<Red>()
+            newTrafficLight.fixture shouldBe 1234
         }
     })
     "a red traffic light that can sometimes go straight to green" should ({
         "when changed once should not skip amber if flag not set" {
-            val redTrafficLight = RedVersion2(shouldSkipAmber = false)
+            val redTrafficLight = RedVersion2(fixture = 1234, shouldSkipAmber = false)
             changeColors(redTrafficLight,1).shouldBeTypeOf <RedOutput.RedAmberOption>()
         }
         "when changed once should skip amber if flag is set" {
-            val redTrafficLight = RedVersion2(shouldSkipAmber = true)
+            val redTrafficLight = RedVersion2(fixture = 1234, shouldSkipAmber = true)
             changeColors(redTrafficLight,1).shouldBeTypeOf <RedOutput.GreenOption>()
         }
         "when changed two times turns green if skip amber flag not set" {
-            val redTrafficLight = RedVersion2(shouldSkipAmber = false)
+            val redTrafficLight = RedVersion2(fixture = 1234, shouldSkipAmber = false)
             changeColors(redTrafficLight,2).shouldBeTypeOf<Green>()
         }
         "when changed two times turns amber if skip amber flag set" {
-            val redTrafficLight = RedVersion2(shouldSkipAmber = true)
+            val redTrafficLight = RedVersion2(fixture = 1234, shouldSkipAmber = true)
             changeColors(redTrafficLight,2).shouldBeTypeOf<Amber>()
+        }
+        "when fixture is zero the traffic light is always broken" {
+            val redTrafficLight = RedVersion2(fixture = 0, shouldSkipAmber = true)
+            val newTrafficLight = changeColors(redTrafficLight,2)
+            newTrafficLight.shouldBeTypeOf<Broken>()
+            newTrafficLight.fixture shouldBe 0
+            newTrafficLight.reason shouldBe "Fixture 0 doesn't work"
         }
 
     })
