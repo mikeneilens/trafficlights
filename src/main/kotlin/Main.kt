ba5dc9564f8a3f835a3b70f8534cf1e5e9d002c7
Mike Neilens <mike.neilens@gmail.com>
2023-03-29 17:35:58 +0100

    initial

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,51 +1,44 @@
-interface TrafficLightState {
-    fun changeColour():TrafficLightState
+interface TypeOfTrafficLight {
+    fun changeColour():TypeOfTrafficLight
 }
 
 //This applies changes to a traffic light by the number of times specified by numberOfChanges
-fun changeColors(trafficLightState: TrafficLightState, numberOfChanges:Int):TrafficLightState =
-     (1..numberOfChanges).fold(trafficLightState){tls, _ -> tls.changeColour() }
+fun changeColors(typeOfTrafficLight: TypeOfTrafficLight, numberOfChanges:Int):TypeOfTrafficLight =
+     (1..numberOfChanges).fold(typeOfTrafficLight){ tls, _ -> tls.changeColour() }
 
-class Red(val serialNumber:Int):TrafficLightState {
-    override fun changeColour():RedAmber = RedAmber(serialNumber)
+class RedTrafficLight(val serialNumber:Int):TypeOfTrafficLight {
+    override fun changeColour():RedOutput =
+        if (serialNumber != 0)
+            RedOutput.RedAmberOption(serialNumber)
+        else
+            RedOutput.BrokenOption(serialNumber, "Light with SerialNumber 0 doesn't work")
+
     override fun toString() = "I am Red"
 }
 
-class RedAmber(val serialNumber:Int):TrafficLightState {
-    override fun changeColour():Green = Green(serialNumber)
+sealed class RedOutput(val typeOfTrafficLight: TypeOfTrafficLight):TypeOfTrafficLight {
+    class RedAmberOption( serialNumber:Int):RedOutput(RedAmberTrafficLight(serialNumber))
+    class BrokenOption( serialNumber:Int, reason:String):RedOutput(BrokenTrafficLight(serialNumber, reason))
+    override fun changeColour(): TypeOfTrafficLight = this.typeOfTrafficLight.changeColour()
+}
+
+class RedAmberTrafficLight(val serialNumber:Int):TypeOfTrafficLight {
+    override fun changeColour():GreenTrafficLight = GreenTrafficLight(serialNumber)
     override fun toString() = "I am Red and Amber"
 }
 
-class Green(val serialNumber:Int):TrafficLightState {
-    override fun changeColour():Amber = Amber(serialNumber)
+class GreenTrafficLight(val serialNumber:Int):TypeOfTrafficLight {
+    override fun changeColour():AmberTrafficLight = AmberTrafficLight(serialNumber)
     override fun toString() = "I am Green"
 }
 
-class Amber(val serialNumber:Int):TrafficLightState {
-    override fun changeColour():Red = Red(serialNumber)
+class AmberTrafficLight(val serialNumber:Int):TypeOfTrafficLight {
+    override fun changeColour():RedTrafficLight = RedTrafficLight(serialNumber)
     override fun toString() = "I am Amber"
 }
 
-
-//If Red had more than one possible output then we could use something like this
-//n.b. Once RedVersion2 has been changed several times it turns back into a standard red traffic light!
-class RedVersion2(val serialNumber:Int, val shouldSkipAmber:Boolean):TrafficLightState {
-    override fun changeColour():RedOutput =
-        if (serialNumber == 0) RedOutput.BrokenOption(serialNumber, "Light with SerialNumber 0 doesn't work")
-        else if (shouldSkipAmber) RedOutput.GreenOption(serialNumber)
-        else RedOutput.RedAmberOption(serialNumber)
-    override fun toString() = "I am Red but can turn to red/amber or go straight to green"
-}
-
-sealed class RedOutput(val trafficLightState: TrafficLightState):TrafficLightState {
-    class RedAmberOption( serialNumber:Int):RedOutput(RedAmber(serialNumber))
-    class GreenOption( serialNumber:Int):RedOutput(Green(serialNumber))
-    class BrokenOption( serialNumber:Int, val reason:String):RedOutput(Broken(serialNumber, reason))
-    override fun changeColour(): TrafficLightState = this.trafficLightState.changeColour()
-}
-
 //Broken is a special case where the actual traffic light can't change colour, e.g. bulbs not working or no power.
-class Broken(val serialNumber:Int, val reason:String):TrafficLightState {
-    override fun changeColour():Broken = Broken(serialNumber, "Still broken")
+class BrokenTrafficLight(val serialNumber:Int, val reason:String):TypeOfTrafficLight {
+    override fun changeColour():BrokenTrafficLight = BrokenTrafficLight(serialNumber, "Still broken")
     override fun toString() = "I am Broken"
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -4,59 +4,47 @@ import io.kotest.matchers.types.shouldBeTypeOf
 
 class MainTest:WordSpec ({
     "a red traffic light" should  ({
-        val redTrafficLight = Red(serialNumber = 1234)
+
+        val redTrafficLight = RedTrafficLight(serialNumber = 1234)
+
         "when changed once should turn red/amber" {
             val newTrafficLight = changeColors(redTrafficLight,1)
-            newTrafficLight.shouldBeTypeOf<RedAmber>()
-            newTrafficLight.serialNumber shouldBe 1234
+            newTrafficLight.shouldBeTypeOf <RedOutput.RedAmberOption>()
+            newTrafficLight.typeOfTrafficLight.shouldBeTypeOf<RedAmberTrafficLight>()
+            (newTrafficLight.typeOfTrafficLight as RedAmberTrafficLight).serialNumber shouldBe 1234
         }
         "when changed two times turns green" {
             val newTrafficLight = changeColors(redTrafficLight,2)
-            newTrafficLight.shouldBeTypeOf<Green>()
+            newTrafficLight.shouldBeTypeOf<GreenTrafficLight>()
             newTrafficLight.serialNumber shouldBe 1234
         }
         "when changed three times turns amber" {
             val newTrafficLight = changeColors(redTrafficLight,3)
-            newTrafficLight.shouldBeTypeOf<Amber>()
+            newTrafficLight.shouldBeTypeOf<AmberTrafficLight>()
             newTrafficLight.serialNumber shouldBe 1234
         }
         "when changed four times turns red" {
             val newTrafficLight = changeColors(redTrafficLight,4)
-            newTrafficLight.shouldBeTypeOf<Red>()
+            newTrafficLight.shouldBeTypeOf<RedTrafficLight>()
             newTrafficLight.serialNumber shouldBe 1234
         }
     })
-    "a red traffic light that can sometimes go straight to green" should ({
-        "when changed once should not skip amber if flag not set" {
-            val redTrafficLight = RedVersion2(serialNumber = 1234, shouldSkipAmber = false)
-            changeColors(redTrafficLight,1).shouldBeTypeOf <RedOutput.RedAmberOption>()
-        }
-        "when changed once should skip amber if flag is set" {
-            val redTrafficLight = RedVersion2(serialNumber = 1234, shouldSkipAmber = true)
-            changeColors(redTrafficLight,1).shouldBeTypeOf <RedOutput.GreenOption>()
-        }
-        "when changed two times turns green if skip amber flag not set" {
-            val redTrafficLight = RedVersion2(serialNumber = 1234, shouldSkipAmber = false)
-            changeColors(redTrafficLight,2).shouldBeTypeOf<Green>()
-        }
-        "when changed two times turns amber if skip amber flag set" {
-            val redTrafficLight = RedVersion2(serialNumber = 1234, shouldSkipAmber = true)
-            changeColors(redTrafficLight,2).shouldBeTypeOf<Amber>()
-        }
-        "when serial number is zero the traffic light is always broken" {
-            val redTrafficLight = RedVersion2(serialNumber = 0, shouldSkipAmber = true)
-            val newTrafficLight = redTrafficLight.changeColour()
-            newTrafficLight.shouldBeTypeOf<RedOutput.BrokenOption>()
-            newTrafficLight.trafficLightState.shouldBeTypeOf<Broken>()
-            (newTrafficLight.trafficLightState as Broken).run {
-                serialNumber shouldBe 0
-                reason shouldBe "Light with SerialNumber 0 doesn't work"
-            }
+    "a red traffic light with serialnumber zero" should  ({
 
-            val trafficLightAfterSecondChange = newTrafficLight.changeColour()
-            trafficLightAfterSecondChange.shouldBeTypeOf<Broken>()
-            trafficLightAfterSecondChange.reason shouldBe "Still broken"
-        }
+        val redTrafficLight = RedTrafficLight(serialNumber = 0)
 
+        "when changed once should produce a broken traffic light" {
+            val newTrafficLight = changeColors(redTrafficLight,1)
+            newTrafficLight.shouldBeTypeOf <RedOutput.BrokenOption>()
+            newTrafficLight.typeOfTrafficLight.shouldBeTypeOf<BrokenTrafficLight>()
+            (newTrafficLight.typeOfTrafficLight as BrokenTrafficLight).serialNumber shouldBe 0
+            (newTrafficLight.typeOfTrafficLight as BrokenTrafficLight).reason shouldBe "Light with SerialNumber 0 doesn't work"
+        }
+        "when changed twice should remain broken" {
+            val newTrafficLight = changeColors(redTrafficLight,2)
+            newTrafficLight.shouldBeTypeOf <BrokenTrafficLight>()
+            newTrafficLight.serialNumber shouldBe 0
+            newTrafficLight.reason shouldBe "Still broken"
+        }
     })
 })
